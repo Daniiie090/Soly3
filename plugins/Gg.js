06a9e2ed3414d0306f388dba816e78501ffd4e23
Daniiie090 <157050551+Daniiie090@users.noreply.github.com>
2024-03-16 13:03:28 +0300

    Delete plugins directory

--- plugins/Gg.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m) => {
-  global.db.data.chats[m.chat].isBanned = true;
-  m.reply('*[❗تنبيه❗] تم حظر هذا الشات*\n\n*—◉ لن يستجيب البوت لأية اوامر*');
-};
-handler.help = ['banchat'];
-handler.tags = ['owner'];
-handler.command = /^احظر$/i;
-handler.rowner = true;
-export default handler;
--- plugins/بنك (1).js
+++
@@ -1,25 +0,0 @@
-let handler = async (m, {conn, usedPrefix}) => {
-	
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let user = global.db.data.users[who]
-    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    conn.reply(m.chat, `
-┌───⊷ *البنك* ⊶
-▢ *📌الاسم* : _@${who.split('@')[0]}_
-▢ *🪙ذهب* : _${user.credit}_
-▢ *🧑الرتبه* : _${user.role}_
-▢*🎖️ المستوى:* ${user.level}
-▢*🧰 الخبرة:* ${user.exp}
-▢*🎟️ مميز:* ${user.premiumTime > 0 ? '✅' : (user.isPrems ? '✅' : '❌') || ''}
-*ملحوظه :* 
-*يمكنك شراء 🪙 الذهب باستخدام الأوامر*
-*يمكنك شراء3 ذهب مقابل الخبرة باستخدام لامر
-❏ *.شراء3 وتضف الكمية*
-❏ *.شراء3 1*
-❏ *ويمكنك مضاعفة الكميه*`, m, { mentions: [who] })
-}
-handler.help = ['balance']
-handler.tags = ['econ']
-handler.command = ['bal', 'البنك', 'diamond', 'balance'] 
-
-export default handler
